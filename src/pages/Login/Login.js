d7381911951995cd0e2d0eade79c18bff4fdd66f
larissa <larissasilva.268@gmail.com>
2020-09-04 21:44:36 -0300

    login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
-import {  Box, Text } from "@chakra-ui/core";
- 
+import { Box, InputLeftElement, Icon, InputGroup, Input } from "@chakra-ui/core";
+
 const Login = () => {
- 
+
   return (
     <Box display="flex" flex={1} flexDirection="column" justifyContent="center" alignItems="center" height="100vh" bg="background">
-      <Text color="secondary">Login</Text>
+      <InputGroup>
+        <InputLeftElement children={<Icon name="email" color="primary" />} />
+        <Input type="email" placeholder="Email" variant="flushed" borderColor="primary" />
+      </InputGroup>
+      <InputGroup>
+        <InputLeftElement children={<Icon name="lock" color="primary" />} />
+        <Input type="password" placeholder="Senha" variant="flushed" borderColor="primary" />
+      </InputGroup>
     </Box>
   );
-  
+
 }
 
 export default Login;
