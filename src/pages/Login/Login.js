65741b07a8d3087c1ef62db66a765c8bb19a9eb5
larissa <larissasilva.268@gmail.com>
2020-09-06 08:36:37 -0300

    Tela de login

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,19 +1,30 @@
 import React from 'react';
 
-import { Box, InputLeftElement, Icon, InputGroup, Input } from "@chakra-ui/core";
+import { Box, InputLeftElement, Icon, InputGroup, Input, Image, Link } from "@chakra-ui/core";
+
+import logo from '../../assets/logo.svg'
 
 const Login = () => {
 
   return (
     <Box display="flex" flex={1} flexDirection="column" justifyContent="center" alignItems="center" height="100vh" bg="background">
-      <InputGroup>
-        <InputLeftElement children={<Icon name="email" color="primary" />} />
-        <Input type="email" placeholder="Email" variant="flushed" borderColor="primary" />
-      </InputGroup>
-      <InputGroup>
-        <InputLeftElement children={<Icon name="lock" color="primary" />} />
-        <Input type="password" placeholder="Senha" variant="flushed" borderColor="primary" />
-      </InputGroup>
+
+      <Image src={logo} alt="Logo Olivia App" width="140px" />
+
+      <Box pt={5}>
+        <InputGroup>
+          <InputLeftElement children={<Icon name="email" color="primary" />} />
+          <Input type="email" placeholder="Email" variant="flushed" borderColor="primary" />
+        </InputGroup>
+        <InputGroup>
+          <InputLeftElement children={<Icon name="lock" color="primary" />} />
+          <Input type="password" placeholder="Senha" variant="flushed" borderColor="primary" />
+        </InputGroup>
+      </Box>
+
+      <Box color="white" display="flex" pt={5}>
+        <Link href="#">Esqueceu a senha?</Link>
+      </Box>
     </Box>
   );
 
