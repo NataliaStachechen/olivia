4fde2ce0f3f4b7544c68d6d3791da78fc50d8178
larissa <larissasilva.268@gmail.com>
2020-09-07 19:51:01 -0300

    Ajusta icons do painel

--- src/pages/panel/Timeline/Timeline.js
+++ src/pages/panel/Timeline/Timeline.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { InputGroup, InputLeftElement, Input, Box, Icon } from '@chakra-ui/core'
+import { InputGroup, InputLeftElement, Input, Box, Icon, Text, Image } from '@chakra-ui/core'
 
 import { CardInfo } from 'components/CardInfo'
 import { CardButton } from 'components/CardButton'
@@ -7,6 +7,11 @@ import { LayoutPanel } from 'components/LayoutPanel'
 
 import alanaAvatar from 'assets/alana_avatar.png'
 import lideranca from 'assets/liderança.svg'
+import collaboration from 'assets/collaboration.svg'
+import manage from 'assets/manage.svg'
+import megaphone from 'assets/megaphone.svg'
+import unchecked from 'assets/unchecked.svg'
+import checked from 'assets/checked.svg'
 
 const Timeline = () => {
   return (
@@ -25,12 +30,44 @@ const Timeline = () => {
         />
       </Box>
 
+      <Text color="#fff" mb={5} fontSize="xl">Olá, Maria!</Text>
       <Box display="flex" flex={1} justifyContent="space-between" flexWrap="wrap">
         <CardButton title="Liderança" image={lideranca} bg="#FF5668" />
-        <CardButton title="Gestão" image={lideranca} bg="#41D5E2" />
-        <CardButton title="Cursos" image={lideranca} bg="#4D53E0" />
-        <CardButton title="Mentorias e Palestras" image={lideranca} bg="#FF8E36" />
+        <CardButton title="Gestão" image={manage} bg="#41D5E2" />
+        <CardButton title="Cursos" image={megaphone} bg="#4D53E0" />
+        <CardButton title="Mentorias e Palestras" image={collaboration} bg="#FF8E36" />
       </Box>
+
+      <Text color="#fff" fontSize="xl" mt={5} mb={5}>Tasks</Text>
+      <Box display="flex" alignItems="center" my="20px" color="white" bg="#404040" borderRadius="12px" padding="12px">
+        <Image src={unchecked} width="20px" />
+        <Text ml={5}>Definir o objetivo</Text>
+      </Box>
+      <Box display="flex" alignItems="center" mb={50} my="20px" color="white" bg="#404040" borderRadius="12px" padding="12px">
+        <Image src={unchecked} width="20px" />
+        <Text ml={5}>Avalie seu mercado</Text>
+      </Box>
+      <Box display="flex" alignItems="center" mb={50} my="20px" color="white" bg="#404040" borderRadius="12px" padding="12px">
+        <Image src={unchecked} width="20px" />
+        <Text ml={5}>Conheça seu público</Text>
+      </Box>
+      <Box display="flex" alignItems="center" mb={50} my="20px" color="white" bg="#404040" borderRadius="12px" padding="12px">
+        <Image src={unchecked} width="20px" />
+        <Text ml={5}>Produza conteúdo</Text>
+      </Box>
+      <Box display="flex" alignItems="center" mb={100} my="20px" color="white" bg="#404040" borderRadius="12px" padding="12px">
+        <Image src={unchecked} width="20px" />
+        <Text ml={5}>Investindo em bons produtos e serviços</Text>
+      </Box>
+      <Box display="flex" alignItems="center" mb={100} my="20px" color="white" bg="#404040" borderRadius="12px" padding="12px">
+        <Image src={checked} width="20px" />
+        <Text ml={5}>Acessar o menu Liderança</Text>
+      </Box>
+      <Box display="flex" alignItems="center" mb={100} my="20px" color="white" bg="#404040" borderRadius="12px" padding="12px">
+        <Image src={checked} width="20px" />
+        <Text ml={5}>Acessar o menu Gestão</Text>
+      </Box>
+
     </LayoutPanel>
   )
 }
